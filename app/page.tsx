c3dc639c46dcc1a1fda3c80d06ab61b6b3db8c7e
farouk chebaiki <farouk.chebaiki@gmail.com>
2025-08-27 05:26:25 +0100

    restarting new nextjs project and adding a dynamic background

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Aurora from './Components/background';
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen fixed top-0 left-0">
+      <Aurora
+        colorStops={["#7CFF67", "#B19EEF", "#5227FF"]}
+        blend={23.5}
+        amplitude={0.7}
+        speed={1}
+        className="w-full h-full"
+      />
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media2.dev.to",
-      },
-    ],
-  }
+  /* config options here */
 };
 
 export default nextConfig;
